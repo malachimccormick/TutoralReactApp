c4045457b877111943ae2f65c1042b960339963a
Malachi Mccormick <malachimccormick@Malachis-MacBook-Pro.local>
2019-01-29 23:05:30 -0500

    version 1

---
+++ src/components/AddTodo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export class AddTodo extends Component {
+    state={
+        title:''
+    }
+    onSubmit=(e)=>{
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({title:''});
+    }
+    onChange = (e) => this.setState({[e.target.name]:e.target.value});
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} style={{display:'flex'}}>
+          <input type='text' name='title' placeholder='Add Todo...'style={{flex:'10', padding:'5px'}}
+           value={this.state.title} onChange={this.onChange}/>
+          <input type='submit' value='Submit'className='btn' style={{flex:'1'}}/>
+      </form>
+    )
+  }
+}
+//PropTypes
+AddTodo.propTypes = {
+    addTodo: PropTypes.func.isRequired,
+    
+}
+
+export default AddTodo
---
+++ src/components/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the todo list app v1.0.0. It is part of a React Crash Course.</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
